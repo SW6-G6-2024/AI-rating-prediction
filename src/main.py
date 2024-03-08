efc58fd35c3602c80adf1299687c4e69a3c15ba1
MortenMunk <mortenmunk97@gmail.com>
2024-03-08 11:29:49 +0100

    init

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from model import get_predicted_ratings
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/ratings/predict", methods=["POST"])
+def predict_ratings():
+    if request.method == "POST":
+        request_data = request.get_json().get('data', "No data found")
+        # use the output of the trained AI model to predict the ratings
+        return get_predicted_ratings(request_data)
 
 
 if __name__ == "__main__":
---
+++ src/model.py
@@ -0,0 +1,5 @@
+from data_formatter import data_formatter
+
+
+def get_predicted_ratings(data):
+    return data_formatter(data)
